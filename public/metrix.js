2a980784bd4870eec02fa7ff9226a0768d7df2b7
Keith McDonnell <keith@dancingtext.com>
2010-04-30 13:44:17 +0100

    [bugfix] forgot to replace dev env js requests

--- public/metrix.js
+++ public/metrix.js
@@ -1,15 +1,15 @@
 var MetrixApp = function (account_code) {
+   
    this.log = function(name, data) {
       // Persist data with tracking pixel GET request to /log
       var image = new Image(1, 1);
       image.onLoad = function () { };
-      
-      // TODO: encode JSON
       event_data = 'account_code=' + account_code + '&name=' + name + '&data=' + data;
-      
-      image.src = 'http://petri.local/log?' + event_data ;
+      image.src = 'http://www.metrixapp.com/log?' + event_data ;
    };
    
-   // TODO:  Uniquely identify user with cookie
-   // TODO:  Uniquely identify user with user identifier
+   // TODO: encode JSON
+   // TODO: handle https requests
+   // TODO: Uniquely identify user with cookie
+   // TODO: Uniquely identify user with user identifier
 };
