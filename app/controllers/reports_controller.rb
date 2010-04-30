cbf4c26bfd6895db7066fe0be67fcc12a0fda66a
Keith McDonnell <keith@dancingtext.com>
2010-04-30 16:09:59 +0100

    CSV download OK

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,15 +5,23 @@ class ReportsController < ApplicationController
   end
   
   def index
-    @events = Event.graph_data_for current_user.account_code
-    
     respond_to do |format|
+      
       format.html do
         redirect_to new_report_path if current_user.blank_slate?
+        setup_graph_data
       end
       
-      format.xml # Load amstock_settings (maybe this should not be publicly available)
-      format.csv  # Render entire dataset as CSV
+      # Load amstock_settings (maybe this should not be publicly available)
+      format.xml { setup_graph_data } 
+      
+      format.csv { Event.all.to_csv(:only => [:created_at, :name, :data]) }
     end
+  end
+  
+  private
+  
+  def setup_graph_data
+    @events ||= Event.graph_data_for current_user.account_code
   end
 end
