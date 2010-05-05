0cce4c2dc830636b888ec43004135e1602553fff
Keith McDonnell <keith@dancingtext.com>
2010-05-05 13:16:18 +0100

    Updated landing page copy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resources :user_sessions
   map.login 'login', :controller => 'user_sessions', :action => 'new'
+  map.signup 'signup', :controller => 'users', :action => 'new'
   
   # Landing (SEO) page
   map.resources :leads
